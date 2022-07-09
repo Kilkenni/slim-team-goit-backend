ca8639f29fa062c5c2b2cf9f872058feabed1b48
Cocokringle <cocokringle.1@gmail.com>
2022-07-09 22:38:21 +0300

    in the process of creating (fixup)

--- controllers/diary/addNewProductToDiary.js
+++ controllers/diary/addNewProductToDiary.js
@@ -3,18 +3,45 @@
 const addNewProductToDiary = async (req, res) => {
     // const { _id } = req.user;
     // const { productName, grams, date} = req.body;
-    // const userDiary = await Diary.find({ $and: [{ date: { $eq: date } }, { owner: { $eq: _id } }] });
-    // if(!userDiary){
-        // create newUserDiary
-        // await Diary.create({
-        //     date: date,
+    // const userDiary = await Diary.find({ $and: [{ date: { $eq: date } }, 
+    //     { owner: { $eq: _id } }] });
+        
+    // if(!userDiary) {
+    //    const newProduct = await Diary.create({
+    //         date: date,
+    //         products:[
+    //             {
+    //                 name: productName,
+    //                 weight: grams,
+    //             }
+    //         ],
+    //         owner: _id,
 
-        // })
-    // }
+    //     })
+
+    //     res.status(201).json({
+    //         status: "Success",
+    //         code: 201,
+    //         message: "New diary entry created, product added.",
+    //         data: {
+    //           result: newProduct,
+    //         },
+    //     });
+    // };
+    
     // if(userDiary){
-        // update old userdiary
+    //    const newProduct = await Diary.updateOne({date: date, owner: _id}, 
+    //     {$push: {products: {name: productName, weight: grams,}} });
+       
+    //    res.status(201).json({
+    //     status: "Success",
+    //     code: 201,
+    //     message: "Diary entry updated, product added.",
+    //     data: {
+    //       result: newProduct,
+    //     },
+    // });
     // }
-
 
 
 }
