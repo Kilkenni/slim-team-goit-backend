46da3fee5f6fe8727eed412ee2090e32103748e6
Roman Lysenko <86610250+Solomon-IT-Dev@users.noreply.github.com>
2022-07-10 19:50:24 +0300

    feature/model (mongoose and joi)

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const app = require("./app");
 const mongoose = require("mongoose");
-const {DB_HOST, PORT=3000} = require("./helpers/env");
-
+const { DB_HOST, PORT = 3000 } = require("./helpers/env");
 
 // Assemble database path from ENV variables. TODO: create new Mongo DB
 // const DB_HOST = `mongodb+srv://${process.env.MONGO_NAME}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}`;
@@ -21,10 +20,3 @@ mongoose
     console.error("ERROR ", err);
     process.exit(1);
   });
-
-// ! Времення до создания БД
-// app.listen(PORT, () => {
-//   console.log(
-//     `Server running. Use our API on port: ${PORT}. Press [Ctrl + C] in terminal to stop it.`
-//   );
-// });
