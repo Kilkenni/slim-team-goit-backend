947491c9bccd8831b65d18181d8a285692f78d81
Olena <86402764+mikhailova11@users.noreply.github.com>
2022-07-10 23:19:59 +0300

    registrate

---
+++ controllers/auth/registration.js
@@ -0,0 +1,19 @@
+const {User} = require('../../models/userSchema')
+const bcrypt = require('bcryptjs')
+
+
+const registration = async (req, res, next) => {
+  const {email, password} = req.body
+  const user =  await User.findOne({email: email});
+
+  if(user){
+    res.status(409).json({"message":"Email in use"})
+   }
+   
+  const hashedPassword = await bcrypt.hash(password, 10)
+  const registerUser = await User.create({... req.body, password: hashedPassword})
+  res.status(201).json(registerUser) 
+}
+
+module.exports = registration
+
