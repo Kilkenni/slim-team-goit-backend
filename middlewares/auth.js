b1d42c2792ec14193d950f7e7d5070c23aff2702
Kilkenni <hpittman89@outlook.com>
2022-07-20 14:22:09 +0300

    handling error in auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,29 +5,29 @@ const { createError } = require("../helpers/errors");
 
 const authenticateUser = async (token) => {
   try {
-      const payload = jwt.verify(token, JWT_ACCESS_SECRET)
-      return await User.findById(payload._id)
+    const payload = jwt.verify(token, JWT_ACCESS_SECRET);
+    return await User.findById(payload._id);
   } catch (error) {
-      return null
+    return null;
   }
+};
+
+const auth = async (req, _, next) => {
+  const { authorization = "" } = req.headers;
+  const [bearer, token] = authorization.split(" ");
+
+  if (bearer !== "Bearer" || !token) {
+    next(createError(401, "Not authorized"));
   }
-  
-  const auth = async (req, _, next) => {
-      const {authorization =""} = req.headers;
-      const [bearer, token] = authorization.split(' ')
-  
-      if(bearer !== 'Bearer' || !token){
-        throw createError(401, "Not authorized")
-      }
-  
-      const user = await authenticateUser(token)
-  
-      if(!user || !user.token){
-        throw createError(401, "Not authorized")
-      }
-      req.user = user
-      next()
+
+  const user = await authenticateUser(token);
+
+  if (!user || !user.token) {
+    next(createError(401, "Not authorized"));
   }
+  req.user = user;
+  next();
+};
 
 // const {  Session } = require("../models");
 
